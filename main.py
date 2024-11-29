d3cd8288c48780c23eb67e0216448eb7a604c53f
Amelie Lheritier <amelie.lheritier@epfl.ch>
2024-11-29 16:29:42 +0100

    rrr

--- main.py
+++ main.py
@@ -4,7 +4,8 @@ from scipy.signal import savgol_filter
 from scipy.stats import zscore
 from sklearn.linear_model import LinearRegression
 from sklearn.model_selection import train_test_split
-from sklearn.metrics import mean_squared_error
+from sklearn.model_selection import KFold, GridSearchCV 
+from sklearn.linear_model import Lasso, Ridge
 
 data = pd.read_csv("./train.csv")
 spectrum = data.iloc[:, 6:]
@@ -16,19 +17,48 @@ spectrum_test = test_data.iloc[:, 6:]
 spectrum_test_filtered = pd.DataFrame(savgol_filter(spectrum, 7, 3, deriv = 2, axis = 0))
 spectrum_test_filtered_st = zscore(spectrum_filtered, axis = 1) 
 """
+"""
+correlation  = data.iloc[:, 6:].corr()
+np.fill_diagonal(correlation, 0)
+df_clean = spectrum_filtered_st.drop(spectrum_filtered_st.columns(np.where(correlation >= 0.99), axis = 1))
+"""
+
+corrwithpurity = data.iloc[:, 6:].corrwith(data['PURITY'])
+spectrum_filtered_st = spectrum_filtered_st.drop(spectrum_filtered_st.columns[np.where(np.abs(corrwithpurity) <= 0.3)[0]], axis=1)
+
+def tune_model(model, data, target):
+
+    # Create a KFold cross-validator with 20 folds
+    kf = KFold(n_splits=20, shuffle=True, random_state=42)
+
+    # Define the parameter grid for tuning alpha
+    param_grid = {
+        'alpha': np.logspace(-30, -1, num =30)
+    }
+
+    # Create a grid search with cross-validation
+    grid_search = GridSearchCV(estimator=model, param_grid=param_grid, cv=kf, scoring='neg_root_mean_squared_error')
+
+    # Fit the grid search to the data
+    grid_search.fit(target, data)
+
+    return grid_search
 
 model = LinearRegression()
 X = spectrum_filtered_st
 y = data['PURITY']
 X_train, X_valid, y_train , y_valid = train_test_split(X, y, test_size=0.05, random_state=42) 
 
-model.fit(X_train,y_train)
+# Tune the LassoRegressor model
+res1_lasso = tune_model(Lasso(max_iter = 10_000), X, y)
 
-y_pred = model.predict(X_valid)
+#model.fit(X_train,y_train)
+
+#y_pred = model.predict(X_valid)
 #rmse = np.sqrt(mean_squared_error(y_valid,y_pred))
 #print(rmse)
 
 #print("Nombre de prédicteurs :", X.shape[1])
 
-t_score = np.mean(np.abs(y_pred - y_valid) <= 5)
+t_score = np.mean(np.abs(res1_lasso - y_valid) <= 5)
 print(t_score)
--- polynomial.py
+++ polynomial.py
@@ -2,21 +2,44 @@ import main as m
 from sklearn.linear_model import LinearRegression
 from sklearn.preprocessing import PolynomialFeatures
 from sklearn.pipeline import make_pipeline
-from sklearn.metrics import mean_squared_error
 from sklearn.model_selection import cross_val_score, GridSearchCV
+from sklearn.model_selection import train_test_split
+from scipy.signal import savgol_filter
+from scipy.stats import zscore
+import numpy as np
+import pandas as pd
+from sklearn.model_selection import RandomizedSearchCV
+
+x_small = m.data.iloc[:100, 6:15]
+spectrum_filtered_small = pd.DataFrame(savgol_filter(x_small, 7, 3, deriv = 2, axis = 0))
+spectrum_filtered_st_small = zscore(spectrum_filtered_small, axis = 1)
+
+X = spectrum_filtered_st_small
+y = m.data['PURITY'][:100]
+X_train, X_valid, y_train , y_valid = train_test_split(X, y, test_size=0.05, random_state=42) 
+print(x_small.shape)
+print(y.shape)
 
 # Create a Polynomial Regression model
 model3 = make_pipeline(PolynomialFeatures(degree=1), LinearRegression())
 param_grid = {'polynomialfeatures__degree': range(1, 9)}
-grid_search = GridSearchCV(model3, param_grid, cv=10,           
-                           scoring='neg_mean_squared_error')
-grid_search.fit(m.X_train, m.y_train)
-best_degree = grid_search.best_params_['polynomialfeatures__degree']
+#grid_search = GridSearchCV(model3, param_grid, cv=10, scoring='neg_mean_squared_error')
+random_search = RandomizedSearchCV(estimator=model3, param_distributions=param_grid, n_iter=8, cv=5)
+random_search.fit(m.X_train, m.y_train)
+
+#grid_search.fit(m.X_train, m.y_train)
+best_degree = random_search.best_params_['polynomialfeatures__degree']
 
 best_model3 = make_pipeline(PolynomialFeatures(degree=best_degree), 
                             LinearRegression())
 best_model3.fit(m.X_train, m.y_train)
 
 y_pred3 = best_model3.predict(m.X_test)
-rmse_poly = mean_squared_error(m.y_test, y_pred3, squared=False)
-f'RMSE for best polynomial regressor: {rmse_poly:.3g}, with degree={best_degree}'
+#rmse_poly = mean_squared_error(m.y_test, y_pred3, squared=False)
+#f'RMSE for best polynomial regressor: {rmse_poly:.3g}, with degree={best_degree}'
+t_score = np.mean(np.abs(m.y_pred - m.y_valid) <= 5)
+print(t_score)
+
+
+
+
